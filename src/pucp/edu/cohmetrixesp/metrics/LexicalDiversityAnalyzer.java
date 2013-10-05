9831d84f4f7dfc0be1a93196438ec119e6332a1d
Andre Quispesaravia <drol.005@gmail.com>
2013-10-05 11:26:40 -0500

    Lexical Diversity TODO: Test

---
+++ src/pucp/edu/cohmetrixesp/metrics/LexicalDiversityAnalyzer.java
@@ -0,0 +1,69 @@
+package pucp.edu.cohmetrixesp.metrics;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+import edu.upc.freeling.Sentence;
+import edu.upc.freeling.Word;
+import pucp.edu.cohmetrixesp.structs.CohParagraph;
+import pucp.edu.cohmetrixesp.structs.CohText;
+import pucp.edu.cohmetrixesp.structs.FreelingWordIterable;
+
+public class LexicalDiversityAnalyzer implements ICohAnalyzer{
+	//TODO: test
+	static private LexicalDiversityAnalyzer instance = new LexicalDiversityAnalyzer();
+	private String TYPE_TOKEN_CONTENT = "LDTTRc";
+	private String TYPE_TOKEN_ALL = "LDTTRa";
+
+	public static LexicalDiversityAnalyzer getInstance() {
+		return instance;
+	}
+
+	private LexicalDiversityAnalyzer() {
+	}
+	
+	@Override
+	public void analyze(Map<String, Double> toFill, CohText text) {
+		toFill.put(TYPE_TOKEN_ALL, typeTokenRatioAll(text));
+
+		toFill.put(TYPE_TOKEN_CONTENT, typeTokenRatioContent(text));
+	}
+	
+	boolean isContentWord(Word w){
+		return true;
+	}
+	
+	public double typeTokenRatioContent (CohText text) {
+		double total = 0;
+		HashSet<String> content = new HashSet<>();
+		for (CohParagraph p: text) {
+			for (Sentence s: p) {
+				for (Word w: new FreelingWordIterable(s)){
+					if (isContentWord(w)) {
+						total++;
+						content.add(w.getForm());
+					}
+				}
+			}
+		}
+		if (total <= 0) return 0;
+		return content.size()/total;
+	}
+	
+	public double typeTokenRatioAll (CohText text) {
+		double total = 0;
+		HashSet<String> content = new HashSet<>();
+		for (CohParagraph p: text) {
+			for (Sentence s: p) {
+				for (Word w: new FreelingWordIterable(s)){
+						total++;
+						content.add(w.getForm());
+				}
+			}
+		}
+		if (total <= 0) return 0;
+		return content.size()/total;
+	}
+	
+}

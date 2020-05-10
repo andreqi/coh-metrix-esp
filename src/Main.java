9b196779397795373e7e9430eb93199aa644374e
feralvam <feralvam@gmail.com>
2020-05-10 23:26:01 +0100

    Minor change to command-line interface.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -8,6 +10,9 @@ import java.util.Map;
 import pucp.edu.classifier.ComplexityClassifier;
 import pucp.edu.cohmetrixesp.metrics.MetricsEngine;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonIOException;
+
 
 public class Main {
 
@@ -18,6 +23,7 @@ public class Main {
 		
 		String inputFile = args[0]; 
 		
+		// Read the input file
 		String inputText;
 		try {
 			inputText = Main.readFile(inputFile, Charset.defaultCharset());
@@ -26,13 +32,24 @@ public class Main {
 			return;
 		}
 		
+		// Process the input file
 	   	Map<String, Map<String, Double>> ans = engine.analyzeClasified(inputText);
 	   	String class_ = classifier.classify(ans);
-	   	Gson gson = new Gson();
 	   	Map<String, Object> ret = new HashMap<String, Object> ();
 	   	ret.put("metrics", ans);
 	   	ret.put("class", class_);
-	   	System.out.println(gson.toJson(ret));
+	   	
+	   	// Generate the output
+	   	Gson gson = new GsonBuilder().setPrettyPrinting().create();
+	   	try(Writer writer = new FileWriter("output.json")) {
+			gson.toJson(ret, writer);
+		} catch (JsonIOException e) {
+			System.out.println("Error writing output file.");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("Error writing output file.");
+			e.printStackTrace();
+		}
 	}
 	
 	static String readFile(String path, Charset encoding) throws IOException {
